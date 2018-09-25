24fc6ea564f3717ff2d64a707017ff7b7f499b4c
Camden Cheek <ccheek22@gmail.com>
2018-09-25 08:59:29 -0400

    Add getter methods for reflection

--- src/metrics/metric_types.go
+++ src/metrics/metric_types.go
@@ -1,6 +1,19 @@
 package metrics
 
+import (
+	"reflect"
+)
+
 type QueryDefinition struct {
 	query      string
 	dataModels interface{}
+}
+
+func (qd QueryDefinition) GetQuery() string {
+	return qd.query
+}
+
+func (qd QueryDefinition) GetDataModels() interface{} {
+	ptr := reflect.New(reflect.ValueOf(qd.dataModels).Elem().Type())
+	return ptr.Interface()
 }
