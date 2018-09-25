2116bba24ca307995e62edf596158edac9be5893
Camden Cheek <ccheek22@gmail.com>
2018-09-25 08:59:46 -0400

    Actually collect instance stuff

--- src/metrics/metrics.go
+++ src/metrics/metrics.go
@@ -4,7 +4,9 @@ import (
 	"github.com/blang/semver"
 	"github.com/newrelic/infra-integrations-sdk/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/integration"
+	"github.com/newrelic/infra-integrations-sdk/log"
 	"github.com/newrelic/nri-postgresql/src/connection"
+	"reflect"
 )
 
 func PopulateInstanceMetrics(instanceEntity *integration.Entity, version semver.Version, connection *connection.PGSQLConnection) {
@@ -13,6 +15,20 @@ func PopulateInstanceMetrics(instanceEntity *integration.Entity, version semver.
 		metric.Attribute{Key: "entityName", Value: instanceEntity.Metadata.Namespace + ":" + instanceEntity.Metadata.Name},
 	)
 
-	println(len(metricSet.Metrics))
+	for _, queryDef := range generateInstanceDefinitions(version) {
+		dataModels := queryDef.GetDataModels()
+		if err := connection.Query(dataModels, queryDef.GetQuery()); err != nil {
+			log.Error("Could not execute instance query: %s", err.Error())
+			continue
+		}
+
+		vp := reflect.Indirect(reflect.ValueOf(dataModels))
+		vpInterface := vp.Index(0).Interface()
+		err := metricSet.MarshalMetrics(vpInterface)
+		if err != nil {
+			log.Error("Could not parse metrics from instance query result: %s", err.Error())
+		}
+
+	}
 	// TODO collect instance metrics based on version
 }
