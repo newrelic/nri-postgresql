2020af394f24bed9bf7d8e1f1ea83fa3babeca92
Camden Cheek <ccheek22@gmail.com>
2019-03-19 11:12:22 -0400

    Fix index size querying

--- src/metrics/index_definitions.go
+++ src/metrics/index_definitions.go
@@ -43,13 +43,13 @@ var indexDefinition = &QueryDefinition{
 				t.schemaname as schema_name,
 					t.tablename as table_name,
 					indexname as index_name,
-					pg_relation_size(indexrelname) AS index_size,
+					pg_relation_size(foo.indexoid) AS index_size,
 					idx_tup_read AS tuples_read,
 					idx_tup_fetch AS tuples_fetched
 			FROM pg_tables t
 			LEFT OUTER JOIN pg_class c ON t.tablename=c.relname
 			LEFT OUTER JOIN
-					( SELECT c.relname AS ctablename, ipg.relname AS indexname, x.indnatts AS number_of_columns, idx_scan, idx_tup_read, idx_tup_fetch, indexrelname, indisunique FROM pg_index x
+					( SELECT c.relname AS ctablename, x.indrelid indexoid, ipg.relname AS indexname, x.indnatts AS number_of_columns, idx_scan, idx_tup_read, idx_tup_fetch, indexrelname, indisunique FROM pg_index x
 								 JOIN pg_class c ON c.oid = x.indrelid
 								 JOIN pg_class ipg ON ipg.oid = x.indexrelid
 								 JOIN pg_stat_all_indexes psai ON x.indexrelid = psai.indexrelid )
