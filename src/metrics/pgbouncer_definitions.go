30051cdce475f0432849d5dfc51473e1f174c98f
Camden Cheek <camden@ccheek.com>
2020-07-01 16:27:17 -0400

    Add new pgbouncer metrics

--- src/metrics/pgbouncer_definitions.go
+++ src/metrics/pgbouncer_definitions.go
@@ -19,14 +19,17 @@ var pgbouncerStatsDefinition = &QueryDefinition{
 		TotalReceived   *int `db:"total_received"    metric_name:"pgbouncer.stats.bytesInPerSecond"                                source_type:"rate"`
 		TotalSent       *int `db:"total_sent"        metric_name:"pgbouncer.stats.bytesOutPerSecond"                               source_type:"rate"`
 		TotalXactTime   *int `db:"total_xact_time"   metric_name:"pgbouncer.stats.totalTransactionDurationInMillisecondsPerSecond" source_type:"rate"`
-		TotalQueryTime  *int `db:"total_query_time"  metric_name:"pgbouncer.stats.totalQueryDurationInMillisecondsPerSecond"                source_type:"rate"`
+		TotalQueryTime  *int `db:"total_query_time"  metric_name:"pgbouncer.stats.totalQueryDurationInMillisecondsPerSecond"       source_type:"rate"`
+		TotalRequests   *int `db:"total_requests"    metric_name:"pgbouncer.stats.requestsPerSecond"                               source_type:"rate"`
 		TotalWaitTime   *int `db:"total_wait_time"`
 		AvgXactCount    *int `db:"avg_xact_count"    metric_name:"pgbouncer.stats.avgTransactionCount"                             source_type:"gauge"`
 		AvgXactTime     *int `db:"avg_xact_time"     metric_name:"pgbouncer.stats.avgTransactionDurationInMilliseconds"            source_type:"gauge"`
 		AvgQueryCount   *int `db:"avg_query_count"   metric_name:"pgbouncer.stats.avgQueryCount"                                   source_type:"gauge"`
 		AvgRecv         *int `db:"avg_recv"          metric_name:"pgbouncer.stats.avgBytesIn"                                      source_type:"gauge"`
 		AvgSent         *int `db:"avg_sent"          metric_name:"pgbouncer.stats.avgBytesOut"                                     source_type:"gauge"`
+		AvgReq          *int `db:"avg_req"           metric_name:"pgbouncer.stats.avgRequestsPerSecond"                            source_type:"gauge"`
 		AvgQueryTime    *int `db:"avg_query_time"    metric_name:"pgbouncer.stats.avgQueryDurationInMilliseconds"                  source_type:"gauge"`
+		AvgQuery        *int `db:"avg_query"         metric_name:"pgbouncer.stats.avgQueryDurationInMilliseconds"                  source_type:"gauge"`
 		AvgWaitTime     *int `db:"avg_wait_time"`
 	}{},
 }
