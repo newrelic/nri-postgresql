eba97354a1391c1a7b12e723d94dde134584155f
Joan Porta <jportasa@gmail.com>
2020-10-21 16:48:48 +0200

    Change env vars and use gh instead of hub

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -e
 #
 #
 # Upload dist artifacts to GH Release assets
 #
 #
 cd dist
-for package in $(find  -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)");do
-  echo "===> Uploading to GH $TAG: ${package}"
-  hub release edit -a ${package} -m "${TAG}" ${TAG}
+for filename in $(find  -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)");do
+  echo "===> Uploading to GH $TAG: ${filename}"
+      gh release upload $TAG $filename
 done
+
+
