7661fcb7f99a6d12e5f7d0103144ee240b38255d
ivancorrales <ivan.corrales.solera@gmail.com>
2021-05-10 15:58:35 +0200

    fix: fix detected issue in windows scripts

--- build/windows/set_exe_properties.sh
+++ build/windows/set_exe_properties.sh
@@ -32,6 +32,6 @@ sed \
   -e "s/{Year}/$Year/g" \
   -e "s/{Integration}/nri-$INTEGRATION/g" \
   -e "s/{IntegrationExe}/$INTEGRATION_EXE/g" \
-   ./build/windows/versioninfo.json.template > ./cmd/nri-postgresql/versioninfo.json
+   ./build/windows/versioninfo.json.template > ./cmd/nri-${INTEGRATION}/versioninfo.json
 
-go generate github.com/newrelic/nri-${INTEGRATION}/src/
+go generate github.com/newrelic/nri-${INTEGRATION}/cmd/nri-${INTEGRATION}/
