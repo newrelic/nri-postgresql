940d48639e8c89de7311e2cc6e0d5def76b8082b
Carlos Castro <ccastro@newrelic.com>
2021-06-16 15:00:07 +0100

    Max connections (#66)
    Adding MaxConnection metric to allow calculating percentage of free connections when used together with activeConnections
    
    
    Updating integrations tests

--- src/metrics/database_definitions.go
+++ src/metrics/database_definitions.go
@@ -42,9 +42,12 @@ func (q *QueryDefinition) insertDatabaseNames(databases collection.DatabaseList)
 	return q
 }
 
+// databaseDefinitionUnder91 is the query used to fetch metrics from Postgres below version 9.1.
+// As a special case, max_connections is obtained from the pg_settings table rather than from pg_stat_database
 var databaseDefinitionUnder91 = &QueryDefinition{
 	query: `SELECT -- UNDER91
 		D.datname AS database,
+		(SELECT setting::INTEGER FROM pg_settings WHERE  name = 'max_connections') AS max_connections,
 		SD.numbackends AS active_connections,
 		SD.xact_commit AS transactions_committed,
 		SD.xact_rollback AS transactions_rolled_back,
@@ -64,6 +67,7 @@ var databaseDefinitionUnder91 = &QueryDefinition{
 
 	dataModels: []struct {
 		databaseBase
+		MaxConnections         *int `db:"max_connections"          metric_name:"db.maxconnections"        source_type:"gauge"`
 		ActiveConnections      *int `db:"active_connections"       metric_name:"db.connections"           source_type:"gauge"`
 		TransactionsCommitted  *int `db:"transactions_committed"   metric_name:"db.commitsPerSecond"      source_type:"rate"`
 		TransactionsRolledBack *int `db:"transactions_rolled_back" metric_name:"db.rollbacksPerSecond"    source_type:"rate"`
@@ -77,9 +81,12 @@ var databaseDefinitionUnder91 = &QueryDefinition{
 	}{},
 }
 
+// databaseDefinitionOver91 is the query used to fetch metrics from Postgres version 9.1 and above.
+// As a special case, max_connections is obtained from the pg_settings table rather than from pg_stat_database
 var databaseDefinitionOver91 = &QueryDefinition{
 	query: `SELECT 
 		D.datname AS database,
+		(SELECT setting::INTEGER FROM pg_settings WHERE  name = 'max_connections') AS max_connections,
 		SD.numbackends AS active_connections,
 		SD.xact_commit AS transactions_committed,
 		SD.xact_rollback AS transactions_rolled_back,
@@ -105,6 +112,7 @@ var databaseDefinitionOver91 = &QueryDefinition{
 
 	dataModels: []struct {
 		databaseBase
+		MaxConnections                    *int `db:"max_connections"                             metric_name:"db.maxconnections"                source_type:"gauge"`
 		ActiveConnections                 *int `db:"active_connections"                          metric_name:"db.connections"                   source_type:"gauge"`
 		TransactionsCommitted             *int `db:"transactions_committed"                      metric_name:"db.commitsPerSecond"              source_type:"rate"`
 		TransactionsRolledBack            *int `db:"transactions_rolled_back"                    metric_name:"db.rollbacksPerSecond"            source_type:"rate"`
@@ -123,6 +131,7 @@ var databaseDefinitionOver91 = &QueryDefinition{
 	}{},
 }
 
+// databaseDefinitionOver92 is the query used to fetch extra metrics from Postgres version 9.2 and above.
 var databaseDefinitionOver92 = &QueryDefinition{
 	query: `SELECT 
 		D.datname AS database,
