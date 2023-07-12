e497eadd8211cac1ed0dd59bbe6fa2821868d21e
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2023-07-12 12:53:56 +0200

    Add pools metrics from pgbouncer v1.18 (NR-141120) (#121)
    * Add pools metrics from pgbouncer v1.18
    
    * fix linting issue
    
    * fix from review

--- src/metrics/metrics_test.go
+++ src/metrics/metrics_test.go
@@ -435,91 +435,179 @@ func Test_populateIndexMetricsForDatabase_noIndexes(t *testing.T) {
 }
 
 func TestPopulatePgBouncerMetrics(t *testing.T) {
-	testIntegration, _ := integration.New("test", "test")
-
-	testConnection, mock := connection.CreateMockSQL(t)
-	pgbouncerStatsRows := sqlmock.NewRows([]string{
-		"database",
-		"total_xact_count",
-		"total_query_count",
-		"total_received",
-		"total_sent",
-		"total_xact_time",
-		"total_query_time",
-		"total_wait_time",
-		"avg_xact_count",
-		"avg_xact_time",
-		"avg_query_count",
-		"avg_recv",
-		"avg_sent",
-		"avg_query_time",
-		"avg_wait_time",
-	}).AddRow("testDB", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
-
-	pgbouncerPoolsRows := sqlmock.NewRows([]string{
-		"database",
-		"user",
-		"cl_active",
-		"cl_waiting",
-		"sv_active",
-		"sv_idle",
-		"sv_used",
-		"sv_tested",
-		"sv_login",
-		"maxwait",
-		"maxwait_us",
-		"pool_mode",
-	}).AddRow("testDB", "testUser", 1, 2, 3, 4, 5, 6, 7, 8, 9, "testMode")
-
-	mock.ExpectQuery("SHOW STATS;").
-		WillReturnRows(pgbouncerStatsRows)
-	mock.ExpectQuery("SHOW POOLS;").
-		WillReturnRows(pgbouncerPoolsRows)
-
-	ci := &connection.MockInfo{}
-	PopulatePgBouncerMetrics(testIntegration, testConnection, ci)
-
-	expectedStats := map[string]interface{}{
-		"pgbouncer.stats.transactionsPerSecond":                           float64(0),
-		"pgbouncer.stats.queriesPerSecond":                                float64(0),
-		"pgbouncer.stats.bytesInPerSecond":                                float64(0),
-		"pgbouncer.stats.bytesOutPerSecond":                               float64(0),
-		"pgbouncer.stats.totalTransactionDurationInMillisecondsPerSecond": float64(0),
-		"pgbouncer.stats.totalQueryDurationInMillisecondsPerSecond":       float64(0),
-		"pgbouncer.stats.avgTransactionCount":                             float64(8),
-		"pgbouncer.stats.avgTransactionDurationInMilliseconds":            float64(9),
-		"pgbouncer.stats.avgQueryCount":                                   float64(10),
-		"pgbouncer.stats.avgBytesIn":                                      float64(11),
-		"pgbouncer.stats.avgBytesOut":                                     float64(12),
-		"pgbouncer.stats.avgQueryDurationInMilliseconds":                  float64(13),
-
-		"displayName": "testDB",
-		"entityName":  "pgbouncer:testDB",
-		"event_type":  "PgBouncerSample",
-		"host":        "testhost",
+	testsCases := []struct {
+		name               string
+		pgbouncerPoolsRows *sqlmock.Rows
+		expectedPool       map[string]interface{}
+	}{
+		{
+			name: "pgbouncer version =< 15",
+			pgbouncerPoolsRows: sqlmock.NewRows([]string{
+				"database",
+				"user",
+				"cl_active",
+				"cl_waiting",
+				"sv_active",
+				"sv_idle",
+				"sv_used",
+				"sv_tested",
+				"sv_login",
+				"maxwait",
+				"maxwait_us",
+				"pool_mode",
+			}).AddRow("testDB", "testUser", 1, 2, 3, 4, 5, 6, 7, 8, 9, "testMode"),
+			expectedPool: map[string]interface{}{
+				"pgbouncer.pools.clientConnectionsActive":  float64(1),
+				"pgbouncer.pools.clientConnectionsWaiting": float64(2),
+				"pgbouncer.pools.serverConnectionsActive":  float64(3),
+				"pgbouncer.pools.serverConnectionsIdle":    float64(4),
+				"pgbouncer.pools.serverConnectionsUsed":    float64(5),
+				"pgbouncer.pools.serverConnectionsTested":  float64(6),
+				"pgbouncer.pools.serverConnectionsLogin":   float64(7),
+				"pgbouncer.pools.maxwaitInMilliseconds":    float64(8),
+				"displayName":                              "testDB",
+				"entityName":                               "pgbouncer:testDB",
+				"event_type":                               "PgBouncerSample",
+				"host":                                     "testhost",
+			},
+		},
+		{
+			name: "pgbouncer version 16,17",
+			pgbouncerPoolsRows: sqlmock.NewRows([]string{
+				"database",
+				"user",
+				"cl_active",
+				"cl_waiting",
+				"sv_active",
+				"sv_idle",
+				"sv_used",
+				"sv_tested",
+				"sv_login",
+				"maxwait",
+				"maxwait_us",
+				"pool_mode",
+				"cl_cancel_req", // Added column.
+			}).AddRow("testDB", "testUser", 1, 2, 3, 4, 5, 6, 7, 8, 9, "testMode", 10),
+			expectedPool: map[string]interface{}{
+				"pgbouncer.pools.clientConnectionsActive":    float64(1),
+				"pgbouncer.pools.clientConnectionsWaiting":   float64(2),
+				"pgbouncer.pools.serverConnectionsActive":    float64(3),
+				"pgbouncer.pools.serverConnectionsIdle":      float64(4),
+				"pgbouncer.pools.serverConnectionsUsed":      float64(5),
+				"pgbouncer.pools.serverConnectionsTested":    float64(6),
+				"pgbouncer.pools.serverConnectionsLogin":     float64(7),
+				"pgbouncer.pools.maxwaitInMilliseconds":      float64(8),
+				"displayName":                                "testDB",
+				"entityName":                                 "pgbouncer:testDB",
+				"event_type":                                 "PgBouncerSample",
+				"host":                                       "testhost",
+				"pgbouncer.pools.clientConnectionsCancelReq": float64(10),
+			},
+		},
+		{
+			name: "pgbouncer version => 18",
+			pgbouncerPoolsRows: sqlmock.NewRows([]string{
+				"database",
+				"user",
+				"cl_active",
+				"cl_waiting",
+				"sv_active",
+				"sv_idle",
+				"sv_used",
+				"sv_tested",
+				"sv_login",
+				"maxwait",
+				"maxwait_us",
+				"pool_mode",
+				"cl_waiting_cancel_req",
+				"cl_active_cancel_req",
+				"sv_active_cancel",
+				"sv_being_canceled",
+			}).AddRow("testDB", "testUser", 1, 2, 3, 4, 5, 6, 7, 8, 9, "testMode", 10, 11, 12, 13),
+			expectedPool: map[string]interface{}{
+				"pgbouncer.pools.clientConnectionsActive":           float64(1),
+				"pgbouncer.pools.clientConnectionsWaiting":          float64(2),
+				"pgbouncer.pools.serverConnectionsActive":           float64(3),
+				"pgbouncer.pools.serverConnectionsIdle":             float64(4),
+				"pgbouncer.pools.serverConnectionsUsed":             float64(5),
+				"pgbouncer.pools.serverConnectionsTested":           float64(6),
+				"pgbouncer.pools.serverConnectionsLogin":            float64(7),
+				"pgbouncer.pools.maxwaitInMilliseconds":             float64(8),
+				"displayName":                                       "testDB",
+				"entityName":                                        "pgbouncer:testDB",
+				"event_type":                                        "PgBouncerSample",
+				"host":                                              "testhost",
+				"pgbouncer.pools.clientConnectionsWaitingCancelReq": float64(10),
+				"pgbouncer.pools.clientConnectionsActiveCancelReq":  float64(11),
+				"pgbouncer.pools.serverConnectionsActiveCancel":     float64(12),
+				"pgbouncer.pools.serverConnectionsBeingCancel":      float64(13),
+			},
+		},
 	}
 
-	expectedPool := map[string]interface{}{
-		"pgbouncer.pools.clientConnectionsActive":  float64(1),
-		"pgbouncer.pools.clientConnectionsWaiting": float64(2),
-		"pgbouncer.pools.serverConnectionsActive":  float64(3),
-		"pgbouncer.pools.serverConnectionsIdle":    float64(4),
-		"pgbouncer.pools.serverConnectionsUsed":    float64(5),
-		"pgbouncer.pools.serverConnectionsTested":  float64(6),
-		"pgbouncer.pools.serverConnectionsLogin":   float64(7),
-		"pgbouncer.pools.maxwaitInMilliseconds":    float64(8),
-		"displayName":                              "testDB",
-		"entityName":                               "pgbouncer:testDB",
-		"event_type":                               "PgBouncerSample",
-		"host":                                     "testhost",
+	for _, testCase := range testsCases {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			testIntegration, _ := integration.New("test", "test")
+
+			testConnection, mock := connection.CreateMockSQL(t)
+
+			pgbouncerStatsRows := sqlmock.NewRows([]string{
+				"database",
+				"total_xact_count",
+				"total_query_count",
+				"total_received",
+				"total_sent",
+				"total_xact_time",
+				"total_query_time",
+				"total_wait_time",
+				"avg_xact_count",
+				"avg_xact_time",
+				"avg_query_count",
+				"avg_recv",
+				"avg_sent",
+				"avg_query_time",
+				"avg_wait_time",
+			}).AddRow("testDB", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+
+			expectedStats := map[string]interface{}{
+				"pgbouncer.stats.transactionsPerSecond":                           float64(0),
+				"pgbouncer.stats.queriesPerSecond":                                float64(0),
+				"pgbouncer.stats.bytesInPerSecond":                                float64(0),
+				"pgbouncer.stats.bytesOutPerSecond":                               float64(0),
+				"pgbouncer.stats.totalTransactionDurationInMillisecondsPerSecond": float64(0),
+				"pgbouncer.stats.totalQueryDurationInMillisecondsPerSecond":       float64(0),
+				"pgbouncer.stats.avgTransactionCount":                             float64(8),
+				"pgbouncer.stats.avgTransactionDurationInMilliseconds":            float64(9),
+				"pgbouncer.stats.avgQueryCount":                                   float64(10),
+				"pgbouncer.stats.avgBytesIn":                                      float64(11),
+				"pgbouncer.stats.avgBytesOut":                                     float64(12),
+				"pgbouncer.stats.avgQueryDurationInMilliseconds":                  float64(13),
+
+				"displayName": "testDB",
+				"entityName":  "pgbouncer:testDB",
+				"event_type":  "PgBouncerSample",
+				"host":        "testhost",
+			}
+
+			mock.ExpectQuery("SHOW STATS;").
+				WillReturnRows(pgbouncerStatsRows)
+			mock.ExpectQuery("SHOW POOLS;").
+				WillReturnRows(testCase.pgbouncerPoolsRows)
+
+			ci := &connection.MockInfo{}
+			PopulatePgBouncerMetrics(testIntegration, testConnection, ci)
+
+			id3 := integration.NewIDAttribute("host", "testhost")
+			id4 := integration.NewIDAttribute("port", "1234")
+			pbEntity, err := testIntegration.Entity("testDB", "pgbouncer", id3, id4)
+			assert.Nil(t, err)
+			assert.Equal(t, expectedStats, pbEntity.Metrics[0].Metrics)
+			assert.Equal(t, testCase.expectedPool, pbEntity.Metrics[1].Metrics)
+		})
 	}
 
-	id3 := integration.NewIDAttribute("host", "testhost")
-	id4 := integration.NewIDAttribute("port", "1234")
-	pbEntity, err := testIntegration.Entity("testDB", "pgbouncer", id3, id4)
-	assert.Nil(t, err)
-	assert.Equal(t, expectedStats, pbEntity.Metrics[0].Metrics)
-	assert.Equal(t, expectedPool, pbEntity.Metrics[1].Metrics)
 }
 
 func TestPopulateMetrics(t *testing.T) {
--- src/metrics/pgbouncer_definitions.go
+++ src/metrics/pgbouncer_definitions.go
@@ -38,17 +38,21 @@ var pgbouncerPoolsDefinition = &QueryDefinition{
 
 	dataModels: []struct {
 		databaseBase
-		User        *string `db:"user"`
-		ClCancelReq *int64  `db:"cl_cancel_req"  metric_name:"pgbouncer.pools.clientConnectionsCancelReq" source_type:"gauge"`
-		ClActive    *int64  `db:"cl_active"      metric_name:"pgbouncer.pools.clientConnectionsActive"    source_type:"gauge"`
-		ClWaiting   *int64  `db:"cl_waiting"     metric_name:"pgbouncer.pools.clientConnectionsWaiting"   source_type:"gauge"`
-		SvActive    *int64  `db:"sv_active"      metric_name:"pgbouncer.pools.serverConnectionsActive"    source_type:"gauge"`
-		SvIdle      *int64  `db:"sv_idle"        metric_name:"pgbouncer.pools.serverConnectionsIdle"      source_type:"gauge"`
-		SvUsed      *int64  `db:"sv_used"        metric_name:"pgbouncer.pools.serverConnectionsUsed"      source_type:"gauge"`
-		SvTested    *int64  `db:"sv_tested"      metric_name:"pgbouncer.pools.serverConnectionsTested"    source_type:"gauge"`
-		SvLogin     *int64  `db:"sv_login"       metric_name:"pgbouncer.pools.serverConnectionsLogin"     source_type:"gauge"`
-		MaxWait     *int64  `db:"maxwait"        metric_name:"pgbouncer.pools.maxwaitInMilliseconds"      source_type:"gauge"`
-		MaxWaitUs   *int64  `db:"maxwait_us"`
-		PoolMode    *string `db:"pool_mode"`
+		User               *string `db:"user"`
+		ClCancelReq        *int64  `db:"cl_cancel_req"         metric_name:"pgbouncer.pools.clientConnectionsCancelReq"        source_type:"gauge"` // removed in v1.18
+		ClActive           *int64  `db:"cl_active"             metric_name:"pgbouncer.pools.clientConnectionsActive"           source_type:"gauge"`
+		ClWaiting          *int64  `db:"cl_waiting"            metric_name:"pgbouncer.pools.clientConnectionsWaiting"          source_type:"gauge"`
+		ClWaitingCancelReq *int64  `db:"cl_waiting_cancel_req" metric_name:"pgbouncer.pools.clientConnectionsWaitingCancelReq" source_type:"gauge"` // added in v1.18
+		ClActiveCancelReq  *int64  `db:"cl_active_cancel_req"  metric_name:"pgbouncer.pools.clientConnectionsActiveCancelReq"  source_type:"gauge"` // added in v1.18
+		SvActiveCancel     *int64  `db:"sv_active_cancel"      metric_name:"pgbouncer.pools.serverConnectionsActiveCancel"     source_type:"gauge"` // added in v1.18
+		SvBeingCancel      *int64  `db:"sv_being_canceled"     metric_name:"pgbouncer.pools.serverConnectionsBeingCancel"      source_type:"gauge"` // added in v1.18
+		SvActive           *int64  `db:"sv_active"             metric_name:"pgbouncer.pools.serverConnectionsActive"           source_type:"gauge"`
+		SvIdle             *int64  `db:"sv_idle"               metric_name:"pgbouncer.pools.serverConnectionsIdle"             source_type:"gauge"`
+		SvUsed             *int64  `db:"sv_used"               metric_name:"pgbouncer.pools.serverConnectionsUsed"             source_type:"gauge"`
+		SvTested           *int64  `db:"sv_tested"             metric_name:"pgbouncer.pools.serverConnectionsTested"           source_type:"gauge"`
+		SvLogin            *int64  `db:"sv_login"              metric_name:"pgbouncer.pools.serverConnectionsLogin"            source_type:"gauge"`
+		MaxWait            *int64  `db:"maxwait"               metric_name:"pgbouncer.pools.maxwaitInMilliseconds"             source_type:"gauge"`
+		MaxWaitUs          *int64  `db:"maxwait_us"`
+		PoolMode           *string `db:"pool_mode"`
 	}{},
 }
