c1c28a7aae7877d1edd4140e7e38000e9230d31e
rahul <rannareddy@newrelic.com>
2024-11-06 14:04:13 +0530

    Added seperate test for out of order Simplified version specific tests

--- src/metrics/instance_definitions_test.go
+++ src/metrics/instance_definitions_test.go
@@ -12,49 +12,36 @@ func Test_generateInstanceDefinitions(t *testing.T) {
 		name            string
 		version         string
 		expectedQueries []*QueryDefinition
-		errorExpected   bool
 	}{
 		{
 			name:            "PostgreSQL 9.0",
 			version:         "9.0.0",
 			expectedQueries: []*QueryDefinition{instanceDefinitionBase},
-			errorExpected:   false,
 		},
 		{
 			name:            "PostgreSQL 9.1",
 			version:         "9.1.0",
 			expectedQueries: []*QueryDefinition{instanceDefinitionBase, instanceDefinition91},
-			errorExpected:   false,
 		},
 		{
 			name:            "PostgreSQL 9.2",
 			version:         "9.2.0",
 			expectedQueries: []*QueryDefinition{instanceDefinitionBase, instanceDefinition91, instanceDefinition92},
-			errorExpected:   false,
 		},
 		{
 			name:            "PostgreSQL 10.2",
 			version:         "10.2.0",
 			expectedQueries: []*QueryDefinition{instanceDefinitionBase, instanceDefinition91, instanceDefinition92},
-			errorExpected:   false,
 		},
 		{
 			name:            "PostgreSQL 16.4",
 			version:         "16.4.2",
 			expectedQueries: []*QueryDefinition{instanceDefinitionBase, instanceDefinition91, instanceDefinition92},
-			errorExpected:   false,
 		},
 		{
 			name:            "PostgreSQL 17.0",
 			version:         "17.0.0",
 			expectedQueries: []*QueryDefinition{instanceDefinitionBase170, instanceDefinition170, instanceDefinitionInputOutput170},
-			errorExpected:   false,
-		},
-		{
-			name:            "PostgreSQL 17.5 out of order",
-			version:         "17.5.0",
-			expectedQueries: []*QueryDefinition{instanceDefinition170, instanceDefinitionInputOutput170, instanceDefinitionBase170},
-			errorExpected:   true,
 		},
 	}
 
@@ -63,12 +50,19 @@ func Test_generateInstanceDefinitions(t *testing.T) {
 			version := semver.MustParse(tt.version)
 			queryDefinitions := generateInstanceDefinitions(&version)
 			assert.Equal(t, len(tt.expectedQueries), len(queryDefinitions))
-
-			if tt.errorExpected {
-				assert.False(t, assert.ObjectsAreEqual(tt.expectedQueries, queryDefinitions))
-			} else {
-				assert.ElementsMatch(t, tt.expectedQueries, queryDefinitions)
-			}
+			assert.Equal(t, tt.expectedQueries, queryDefinitions)
 		})
 	}
+
+}
+
+func Test_generateInstanceDefinitionsOutOfOrder(t *testing.T) {
+	t.Run("PostgreSQL 17.5 order check", func(t *testing.T) {
+		version := semver.MustParse("17.5.0")
+		queryDefinitions := generateInstanceDefinitions(&version)
+		expectedQueries := []*QueryDefinition{instanceDefinitionInputOutput170, instanceDefinition170, instanceDefinitionBase170}
+
+		// This fails beacuse order is different
+		assert.False(t, assert.ObjectsAreEqual(expectedQueries, queryDefinitions), "Query definitions should be in the correct order")
+	})
 }
