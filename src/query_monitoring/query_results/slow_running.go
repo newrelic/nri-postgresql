8ffb56a3d8d2eb020a6c83c205a3cd6c5056dbe4
tharun0064 <tbalanagu@newrelic.com>
2024-11-29 15:07:05 +0530

    comment log

--- src/query_monitoring/query_results/slow_running.go
+++ src/query_monitoring/query_results/slow_running.go
@@ -112,7 +112,7 @@ func PopulateSlowRunningMetrics(instanceEntity *integration.Entity, conn *connec
 				}
 			}
 
-			log.Info("Metrics set for slow query: %s in database: %s", *model.QueryID, *model.DatabaseName)
+		//	log.Info("Metrics set for slow query: %s in database: %s", *model.QueryID, *model.DatabaseName)
 		}
 	} else {
 		log.Info("Extension 'pg_stat_statements' is not enabled.")
