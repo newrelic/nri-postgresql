231ec7771b572a4c722b8795b2c484cb92770902
jsurampudi <jsurampudi@newrelic.com>
2024-12-02 16:23:39 +0530

    test

--- src/query_monitoring/query_results/slow_running.go
+++ src/query_monitoring/query_results/slow_running.go
@@ -12,43 +12,6 @@ import (
 	"github.com/newrelic/nri-postgresql/src/query_monitoring/validations"
 )
 
-// FetchAndLogSlowRunningQueries fetches slow-running queries and logs the results
-//func FetchAndLogSlowRunningQueries(instanceEntity *integration.Entity, conn *connection.PGSQLConnection) {
-//	var slowQueries []datamodels.SlowRunningQuery
-//
-//	// Execute the slow queries SQL
-//	err := conn.Query(&slowQueries, queries.SlowQueries)
-//	if err != nil {
-//		log.Error("Error fetching slow-running queries: %v", err)
-//		return
-//	}
-//
-//	// Log the results
-//	for _, query := range slowQueries {
-//		log.Info("Slow Query: %+v", query)
-//		//	//	//log.Info("Slow Query: ID=%d, Text=%s, Database=%s, Schema=%s, ExecutionCount=%d, AvgElapsedTimeMs=%.3f, AvgCPUTimeMs=%.3f, AvgDiskReads=%.3f, AvgDiskWrites=%.3f, StatementType=%s, CollectionTimestamp=%s",
-//		//	//	//	*query.QueryID, *query.QueryText, *query.DatabaseName, *query.SchemaName, *query.ExecutionCount, *query.AvgElapsedTimeMs, *query.AvgCPUTimeMs, *query.AvgDiskReads, *query.AvgDiskWrites, *query.StatementType, *query.CollectionTimestamp)
-//	}
-//	// Log the results
-//
-//}
-
-// GetSlowRunningMetrics executes the given query and returns the result
-// func GetSlowRunningMetrics(conn *connection.PGSQLConnection, query string) ([]datamodels.SlowRunningQuery, error) {
-// 	if !validations.CheckPgStatStatementsExtensionEnabled(conn, "pg_stat_statements") {
-// 		log.Info("Extension 'pg_stat_statements' is not enabled.")
-// 		return nil, nil
-// 	}
-// 	var slowQueries []datamodels.SlowRunningQuery
-
-// 	err := conn.Query(&slowQueries, query)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return slowQueries, nil
-// 	//log.Info("slow-running",slowQueries)
-// }
-
 func GetSlowRunningMetrics(conn *connection.PGSQLConnection) ([]datamodels.SlowRunningQuery, error) {
 	var slowQueries []datamodels.SlowRunningQuery
 	var query = queries.SlowQueries
