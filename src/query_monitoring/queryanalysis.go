0831d0e01f9293f20bbd7791da9f6f15a818feca
jsurampudi <jsurampudi@newrelic.com>
2024-12-09 13:52:34 +0530

    test

--- src/query_monitoring/queryanalysis.go
+++ src/query_monitoring/queryanalysis.go
@@ -7,12 +7,11 @@ import (
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/nri-postgresql/src/args"
 	"github.com/newrelic/nri-postgresql/src/connection"
-	"github.com/newrelic/nri-postgresql/src/query_monitoring/queries"
 	"github.com/newrelic/nri-postgresql/src/query_monitoring/query_results"
 )
 
 func RunAnalysis(instanceEntity *integration.Entity, connection *connection.PGSQLConnection, arguments args.ArgumentList) {
-	query_results.PopulateSlowRunningMetrics(instanceEntity, connection, queries.SlowQueries)
+	//query_results.PopulateSlowRunningMetrics(instanceEntity, connection, queries.SlowQueries)
 	query_results.PopulateWaitEventMetrics(instanceEntity, connection)
 	fmt.Println("Query analysis completed.")
 }
