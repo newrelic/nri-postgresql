47104e395421f2ed6c9000f02497119474243dda
tharun0064 <tbalanagu@newrelic.com>
2024-12-11 18:03:10 +0530

    test

--- src/query_monitoring/query_results/blocking_sessions.go
+++ src/query_monitoring/query_results/blocking_sessions.go
@@ -191,7 +191,7 @@ func GetExecutionPlanMetrics(conn *connection.PGSQLConnection, results []datamod
 			continue
 		}
 		log.Info("execPlan", execPlan[0]["Plan"])
-		firstJson, bl := execPlan[0]["Plan"].(map[string]string)
+		firstJson, bl := execPlan[0]["Plan"].(map[string]interface{})
 		if !bl {
 			log.Error("Failed to marshal firstJson: %v", err)
 			continue
