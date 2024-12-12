754d897345bb9e95c6d0e669a835d1a37e3a2620
tharun0064 <tbalanagu@newrelic.com>
2024-12-12 17:00:21 +0530

    test

--- src/query_monitoring/datamodels/query_datamodels.go
+++ src/query_monitoring/datamodels/query_datamodels.go
@@ -41,26 +41,50 @@ type IndividualQuerySearch struct {
 	DatabaseName *string `json:"datname" db:"datname" metric_name:"queryplan.database" source_type:"attribute"`
 }
 
+//type QueryExecutionPlanMetrics struct {
+//	NodeType            string  `json:"Node Type"           metric_name:"executionplan.node_type"             source_type:"attribute"`
+//	StartupCost         float64 `json:"Startup Cost"        metric_name:"executionplan.startup_cost"          source_type:"gauge"`
+//	TotalCost           float64 `json:"Total Cost"          metric_name:"executionplan.total_cost"            source_type:"gauge"`
+//	PlanRows            int64   `json:"Plan Rows"           metric_name:"executionplan.plan_rows"             source_type:"gauge"`
+//	ActualStartupTime   float64 `json:"Actual Startup Time" metric_name:"executionplan.actual_startup_time"   source_type:"gauge"`
+//	ActualTotalTime     float64 `json:"Actual Total Time"   metric_name:"executionplan.actual_total_time"     source_type:"gauge"`
+//	ActualRows          int64   `json:"Actual Rows"         metric_name:"executionplan.actual_rows"           source_type:"gauge"`
+//	ActualLoops         int64   `json:"Actual Loops"        metric_name:"executionplan.actual_loops"          source_type:"gauge"`
+//	SharedHitBlocks     int64   `json:"Shared Hit Blocks"   metric_name:"executionplan.shared_hit_blocks"     source_type:"gauge"`
+//	SharedReadBlocks    int64   `json:"Shared Read Blocks"  metric_name:"executionplan.shared_read_blocks"    source_type:"gauge"`
+//	SharedDirtiedBlocks int64   `json:"Shared Dirtied Blocks" metric_name:"executionplan.shared_dirtied_blocks" source_type:"gauge"`
+//	SharedWrittenBlocks int64   `json:"Shared Written Blocks" metric_name:"executionplan.shared_written_blocks" source_type:"gauge"`
+//	LocalHitBlocks      int64   `json:"Local Hit Blocks"    metric_name:"executionplan.local_hit_blocks"      source_type:"gauge"`
+//	LocalReadBlocks     int64   `json:"Local Read Blocks"   metric_name:"executionplan.local_read_blocks"     source_type:"gauge"`
+//	LocalDirtiedBlocks  int64   `json:"Local Dirtied Blocks" metric_name:"executionplan.local_dirtied_blocks"  source_type:"gauge"`
+//	LocalWrittenBlocks  int64   `json:"Local Written Blocks" metric_name:"executionplan.local_written_blocks"  source_type:"gauge"`
+//	TempReadBlocks      int64   `json:"Temp Read Blocks"    metric_name:"executionplan.temp_read_blocks"      source_type:"gauge"`
+//	TempWrittenBlocks   int64   `json:"Temp Written Blocks" metric_name:"executionplan.temp_written_blocks"   source_type:"gauge"`
+//	DatabaseName        string  `json:"Database"            metric_name:"executionplan.database"              source_type:"attribute"`
+//	QueryText           string  `json:"Query"               metric_name:"executionplan.query"                 source_type:"attribute"`
+//	QueryId             int64   `json:"Query Id"              metric_name:"executionplan.query_id"                   source_type:"gauge"`
+//}
+
 type QueryExecutionPlanMetrics struct {
-	NodeType            string  `json:"Node Type"           metric_name:"executionplan.node_type"             source_type:"attribute"`
-	StartupCost         float64 `json:"Startup Cost"        metric_name:"executionplan.startup_cost"          source_type:"gauge"`
-	TotalCost           float64 `json:"Total Cost"          metric_name:"executionplan.total_cost"            source_type:"gauge"`
-	PlanRows            int64   `json:"Plan Rows"           metric_name:"executionplan.plan_rows"             source_type:"gauge"`
-	ActualStartupTime   float64 `json:"Actual Startup Time" metric_name:"executionplan.actual_startup_time"   source_type:"gauge"`
-	ActualTotalTime     float64 `json:"Actual Total Time"   metric_name:"executionplan.actual_total_time"     source_type:"gauge"`
-	ActualRows          int64   `json:"Actual Rows"         metric_name:"executionplan.actual_rows"           source_type:"gauge"`
-	ActualLoops         int64   `json:"Actual Loops"        metric_name:"executionplan.actual_loops"          source_type:"gauge"`
-	SharedHitBlocks     int64   `json:"Shared Hit Blocks"   metric_name:"executionplan.shared_hit_blocks"     source_type:"gauge"`
-	SharedReadBlocks    int64   `json:"Shared Read Blocks"  metric_name:"executionplan.shared_read_blocks"    source_type:"gauge"`
-	SharedDirtiedBlocks int64   `json:"Shared Dirtied Blocks" metric_name:"executionplan.shared_dirtied_blocks" source_type:"gauge"`
-	SharedWrittenBlocks int64   `json:"Shared Written Blocks" metric_name:"executionplan.shared_written_blocks" source_type:"gauge"`
-	LocalHitBlocks      int64   `json:"Local Hit Blocks"    metric_name:"executionplan.local_hit_blocks"      source_type:"gauge"`
-	LocalReadBlocks     int64   `json:"Local Read Blocks"   metric_name:"executionplan.local_read_blocks"     source_type:"gauge"`
-	LocalDirtiedBlocks  int64   `json:"Local Dirtied Blocks" metric_name:"executionplan.local_dirtied_blocks"  source_type:"gauge"`
-	LocalWrittenBlocks  int64   `json:"Local Written Blocks" metric_name:"executionplan.local_written_blocks"  source_type:"gauge"`
-	TempReadBlocks      int64   `json:"Temp Read Blocks"    metric_name:"executionplan.temp_read_blocks"      source_type:"gauge"`
-	TempWrittenBlocks   int64   `json:"Temp Written Blocks" metric_name:"executionplan.temp_written_blocks"   source_type:"gauge"`
-	DatabaseName        string  `json:"Database"            metric_name:"executionplan.database"              source_type:"attribute"`
-	QueryText           string  `json:"Query"               metric_name:"executionplan.query"                 source_type:"attribute"`
-	QueryId             int64   `json:"Query Id"              metric_name:"executionplan.query_id"                   source_type:"gauge"`
+	NodeType            string  `mapstructure:"Node Type"           json:"Node Type"           metric_name:"executionplan.node_type"             source_type:"attribute"`
+	StartupCost         float64 `mapstructure:"Startup Cost"        json:"Startup Cost"        metric_name:"executionplan.startup_cost"          source_type:"gauge"`
+	TotalCost           float64 `mapstructure:"Total Cost"          json:"Total Cost"          metric_name:"executionplan.total_cost"            source_type:"gauge"`
+	PlanRows            int64   `mapstructure:"Plan Rows"           json:"Plan Rows"           metric_name:"executionplan.plan_rows"             source_type:"gauge"`
+	ActualStartupTime   float64 `mapstructure:"Actual Startup Time" json:"Actual Startup Time" metric_name:"executionplan.actual_startup_time"   source_type:"gauge"`
+	ActualTotalTime     float64 `mapstructure:"Actual Total Time"   json:"Actual Total Time"   metric_name:"executionplan.actual_total_time"     source_type:"gauge"`
+	ActualRows          int64   `mapstructure:"Actual Rows"         json:"Actual Rows"         metric_name:"executionplan.actual_rows"           source_type:"gauge"`
+	ActualLoops         int64   `mapstructure:"Actual Loops"        json:"Actual Loops"        metric_name:"executionplan.actual_loops"          source_type:"gauge"`
+	SharedHitBlocks     int64   `mapstructure:"Shared Hit Blocks"   json:"Shared Hit Blocks"   metric_name:"executionplan.shared_hit_blocks"     source_type:"gauge"`
+	SharedReadBlocks    int64   `mapstructure:"Shared Read Blocks"  json:"Shared Read Blocks"  metric_name:"executionplan.shared_read_blocks"    source_type:"gauge"`
+	SharedDirtiedBlocks int64   `mapstructure:"Shared Dirtied Blocks" json:"Shared Dirtied Blocks" metric_name:"executionplan.shared_dirtied_blocks" source_type:"gauge"`
+	SharedWrittenBlocks int64   `mapstructure:"Shared Written Blocks" json:"Shared Written Blocks" metric_name:"executionplan.shared_written_blocks" source_type:"gauge"`
+	LocalHitBlocks      int64   `mapstructure:"Local Hit Blocks"    json:"Local Hit Blocks"    metric_name:"executionplan.local_hit_blocks"      source_type:"gauge"`
+	LocalReadBlocks     int64   `mapstructure:"Local Read Blocks"   json:"Local Read Blocks"   metric_name:"executionplan.local_read_blocks"     source_type:"gauge"`
+	LocalDirtiedBlocks  int64   `mapstructure:"Local Dirtied Blocks" json:"Local Dirtied Blocks" metric_name:"executionplan.local_dirtied_blocks"  source_type:"gauge"`
+	LocalWrittenBlocks  int64   `mapstructure:"Local Written Blocks" json:"Local Written Blocks" metric_name:"executionplan.local_written_blocks"  source_type:"gauge"`
+	TempReadBlocks      int64   `mapstructure:"Temp Read Blocks"    json:"Temp Read Blocks"    metric_name:"executionplan.temp_read_blocks"      source_type:"gauge"`
+	TempWrittenBlocks   int64   `mapstructure:"Temp Written Blocks" json:"Temp Written Blocks" metric_name:"executionplan.temp_written_blocks"   source_type:"gauge"`
+	DatabaseName        string  `mapstructure:"Database"            json:"Database"            metric_name:"executionplan.database"              source_type:"attribute"`
+	QueryText           string  `mapstructure:"Query"               json:"Query"               metric_name:"executionplan.query"                 source_type:"attribute"`
+	QueryId             int64   `mapstructure:"Query Id"            json:"Query Id"            metric_name:"executionplan.query_id"              source_type:"gauge"`
 }
