00eeff00b2c65175b67d42b0d2797b2e3698d84a
tharun0064 <tbalanagu@newrelic.com>
2024-12-13 17:38:04 +0530

    refactor common util

--- src/query-performance-monitoring/common-utils/ingestion-helpers.go
+++ src/query-performance-monitoring/common-utils/ingestion-helpers.go
@@ -1,6 +1,11 @@
 package common_utils
 
-import "github.com/newrelic/infra-integrations-sdk/v3/data/metric"
+import (
+	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
+	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/datamodels"
+	"reflect"
+)
 
 func SetMetric(metricSet *metric.Set, name string, value interface{}, sourceType string) {
 	switch sourceType {
@@ -11,4 +16,67 @@ func SetMetric(metricSet *metric.Set, name string, value interface{}, sourceType
 	default:
 		metricSet.SetMetric(name, value, metric.GAUGE)
 	}
+}
+
+func IngestMetric(slowQueries []interface{}, instanceEntity *integration.Entity) {
+
+	for _, model := range slowQueries {
+		metricSet := instanceEntity.NewMetricSet("PostgresSlowQueries")
+
+		modelValue := reflect.ValueOf(model)
+		modelType := reflect.TypeOf(model)
+
+		for i := 0; i < modelValue.NumField(); i++ {
+			field := modelValue.Field(i)
+			fieldType := modelType.Field(i)
+			metricName := fieldType.Tag.Get("metric_name")
+			sourceType := fieldType.Tag.Get("source_type")
+
+			if field.Kind() == reflect.Ptr && !field.IsNil() {
+				SetMetric(metricSet, metricName, field.Elem().Interface(), sourceType)
+			} else if field.Kind() != reflect.Ptr {
+				SetMetric(metricSet, metricName, field.Interface(), sourceType)
+			}
+		}
+	}
+}
+
+func IngestSlowQueryMetrics(slowQueryMetrics []datamodels.SlowRunningQuery, instanceEntity *integration.Entity) {
+	slowQueriesInterface := make([]interface{}, len(slowQueryMetrics))
+	for i, v := range slowQueryMetrics {
+		slowQueriesInterface[i] = v
+	}
+	IngestMetric(slowQueriesInterface, instanceEntity)
+}
+
+func IngestWaitEventMetrics(waitEventMetrics []datamodels.WaitEventQuery, instanceEntity *integration.Entity) {
+	waitEventMetricsInterface := make([]interface{}, len(waitEventMetrics))
+	for i, v := range waitEventMetricsInterface {
+		waitEventMetricsInterface[i] = v
+	}
+	IngestMetric(waitEventMetricsInterface, instanceEntity)
+}
+
+func IngestIndividualQueryMetrics(individualQueryMetrics []datamodels.IndividualQuerySearch, instanceEntity *integration.Entity) {
+	individualQueryMetricsInterface := make([]interface{}, len(individualQueryMetrics))
+	for i, v := range individualQueryMetricsInterface {
+		individualQueryMetricsInterface[i] = v
+	}
+	IngestMetric(individualQueryMetricsInterface, instanceEntity)
+}
+
+func IngestExecutionPlanMetrics(executionPlanMetrics []datamodels.QueryExecutionPlanMetrics, instanceEntity *integration.Entity) {
+	executionPlanMetricsInterface := make([]interface{}, len(executionPlanMetrics))
+	for i, v := range executionPlanMetricsInterface {
+		executionPlanMetricsInterface[i] = v
+	}
+	IngestMetric(executionPlanMetricsInterface, instanceEntity)
+}
+
+func IngestBlockIoMetrics(blockIoMetrics []datamodels.BlockingQuery, instanceEntity *integration.Entity) {
+	blockIoMetricsInterface := make([]interface{}, len(blockIoMetrics))
+	for i, v := range blockIoMetricsInterface {
+		blockIoMetricsInterface[i] = v
+	}
+	IngestMetric(blockIoMetricsInterface, instanceEntity)
 }
--- src/query-performance-monitoring/performance-metrics/slow_query_metrics.go
+++ src/query-performance-monitoring/performance-metrics/slow_query_metrics.go
@@ -1,12 +1,10 @@
 package performance_metrics
 
 import (
-	common_utils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
-	performanceDbConnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
-	"reflect"
-
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/infra-integrations-sdk/v3/log"
+	common_utils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
+	performanceDbConnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/datamodels"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/queries"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/validations"
@@ -58,27 +56,27 @@ func PopulateSlowRunningMetrics(instanceEntity *integration.Entity, conn *perfor
 		return nil
 	}
 	log.Info("Populate-slow running: %+v", slowQueries)
-
-	for _, model := range slowQueries {
-		metricSet := instanceEntity.NewMetricSet("PostgresSlowQueries")
-
-		modelValue := reflect.ValueOf(model)
-		modelType := reflect.TypeOf(model)
-
-		for i := 0; i < modelValue.NumField(); i++ {
-			field := modelValue.Field(i)
-			fieldType := modelType.Field(i)
-			metricName := fieldType.Tag.Get("metric_name")
-			sourceType := fieldType.Tag.Get("source_type")
-
-			if field.Kind() == reflect.Ptr && !field.IsNil() {
-				common_utils.SetMetric(metricSet, metricName, field.Elem().Interface(), sourceType)
-			} else if field.Kind() != reflect.Ptr {
-				common_utils.SetMetric(metricSet, metricName, field.Interface(), sourceType)
-			}
-		}
-
-	}
+	common_utils.IngestSlowQueryMetrics(slowQueries, instanceEntity)
+	//for _, model := range slowQueries {
+	//	metricSet := instanceEntity.NewMetricSet("PostgresSlowQueries")
+	//
+	//	modelValue := reflect.ValueOf(model)
+	//	modelType := reflect.TypeOf(model)
+	//
+	//	for i := 0; i < modelValue.NumField(); i++ {
+	//		field := modelValue.Field(i)
+	//		fieldType := modelType.Field(i)
+	//		metricName := fieldType.Tag.Get("metric_name")
+	//		sourceType := fieldType.Tag.Get("source_type")
+	//
+	//		if field.Kind() == reflect.Ptr && !field.IsNil() {
+	//			common_utils.SetMetric(metricSet, metricName, field.Elem().Interface(), sourceType)
+	//		} else if field.Kind() != reflect.Ptr {
+	//			common_utils.SetMetric(metricSet, metricName, field.Interface(), sourceType)
+	//		}
+	//	}
+	//
+	//}
 	return slowQueries
 
 }
