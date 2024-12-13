199d25925a9e6be95d3584e45ba5cd1c7b1d2ae4
tharun0064 <tbalanagu@newrelic.com>
2024-12-13 12:54:11 +0530

    refactor blocking sessions

--- src/query-performance-monitoring/performance-metrics/blocking_sessions.go
+++ src/query-performance-monitoring/performance-metrics/blocking_sessions.go
@@ -1,9 +1,9 @@
 package performance_metrics
 
 import (
-	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/infra-integrations-sdk/v3/log"
+	common_utils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
 	performanceDbConnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/datamodels"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/queries"
@@ -28,68 +28,50 @@ func GetBlockingMetrics(conn *performanceDbConnection.PGSQLConnection) ([]datamo
 		blockingQueries = append(blockingQueries, blockingQuery)
 	}
 
-	for _, query := range blockingQueries {
-		log.Info("Blocking Query: %+v", query)
-	}
 	return blockingQueries, nil
 }
 
-// PopulateSlowRunningMetrics fetches slow-running metrics and populates them into the metric set
 func PopulateBlockingMetrics(instanceEntity *integration.Entity, conn *performanceDbConnection.PGSQLConnection) {
 	isExtensionEnabled, err := validations.CheckPgStatStatementsExtensionEnabled(conn)
 	if err != nil {
 		log.Error("Error executing query: %v", err)
 		return
 	}
-	if isExtensionEnabled {
-		log.Info("Extension 'pg_stat_statements' enabled.")
-		blockingQueries, err := GetBlockingMetrics(conn)
-		if err != nil {
-			log.Error("Error fetching Blocking queries: %v", err)
-			return
-		}
-
-		if len(blockingQueries) == 0 {
-			log.Info("No Blocking queries found.")
-			return
-		}
-		log.Info("Populate Blocking running: %+v", blockingQueries)
-
-		for _, model := range blockingQueries {
-			metricSet := instanceEntity.NewMetricSet("PostgresBlockingQueries")
-
-			modelValue := reflect.ValueOf(model)
-			modelType := reflect.TypeOf(model)
-
-			for i := 0; i < modelValue.NumField(); i++ {
-				field := modelValue.Field(i)
-				fieldType := modelType.Field(i)
-				metricName := fieldType.Tag.Get("metric_name")
-				sourceType := fieldType.Tag.Get("source_type")
-
-				if field.Kind() == reflect.Ptr && !field.IsNil() {
-					setMetric(metricSet, metricName, field.Elem().Interface(), sourceType)
-				} else if field.Kind() != reflect.Ptr {
-					setMetric(metricSet, metricName, field.Interface(), sourceType)
-				}
-			}
-
-			//	log.Info("Metrics set for slow query: %s in database: %s", *model.QueryID, *model.DatabaseName)
-		}
-	} else {
+	if !isExtensionEnabled {
 		log.Info("Extension 'pg_stat_statements' is not enabled.")
 		return
 	}
-
-}
-
-func setMetric(metricSet *metric.Set, name string, value interface{}, sourceType string) {
-	switch sourceType {
-	case `gauge`:
-		metricSet.SetMetric(name, value, metric.GAUGE)
-	case `attribute`:
-		metricSet.SetMetric(name, value, metric.ATTRIBUTE)
-	default:
-		metricSet.SetMetric(name, value, metric.GAUGE)
+	log.Info("Extension 'pg_stat_statements' enabled.")
+	blockingQueries, err := GetBlockingMetrics(conn)
+	if err != nil {
+		log.Error("Error fetching Blocking queries: %v", err)
+		return
 	}
+
+	if len(blockingQueries) == 0 {
+		log.Info("No Blocking queries found.")
+		return
+	}
+	log.Info("Populate Blocking running: %+v", blockingQueries)
+
+	for _, model := range blockingQueries {
+		metricSet := instanceEntity.NewMetricSet("PostgresBlockingQueries")
+
+		modelValue := reflect.ValueOf(model)
+		modelType := reflect.TypeOf(model)
+
+		for i := 0; i < modelValue.NumField(); i++ {
+			field := modelValue.Field(i)
+			fieldType := modelType.Field(i)
+			metricName := fieldType.Tag.Get("metric_name")
+			sourceType := fieldType.Tag.Get("source_type")
+
+			if field.Kind() == reflect.Ptr && !field.IsNil() {
+				common_utils.SetMetric(metricSet, metricName, field.Elem().Interface(), sourceType)
+			} else if field.Kind() != reflect.Ptr {
+				common_utils.SetMetric(metricSet, metricName, field.Interface(), sourceType)
+			}
+		}
+	}
+
 }
