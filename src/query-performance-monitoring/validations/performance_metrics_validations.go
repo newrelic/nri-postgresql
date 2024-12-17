777d3ad209360a39bf9b6dc2cc29f8ed84ddc9a4
tharun0064 <tbalanagu@newrelic.com>
2024-12-17 11:39:30 +0530

    refactor

--- src/query-performance-monitoring/validations/performance_metrics_validations.go
+++ src/query-performance-monitoring/validations/performance_metrics_validations.go
@@ -8,13 +8,12 @@ import (
 
 func isExtensionEnabled(conn *performanceDbConnection.PGSQLConnection, extensionName string) (bool, error) {
 	rows, err := conn.Queryx(fmt.Sprintf("SELECT count(*) FROM pg_extension WHERE extname = '%s'", extensionName))
-	log.Info("Query for validation", fmt.Sprintf("SELECT count(*) FROM pg_extension WHERE extname = '%s'", extensionName))
 	if err != nil {
 		log.Error("Error executing query: ", err.Error())
 		return false, err
 	}
 	defer rows.Close()
-	var isEnable bool
+
 	var count int
 	for rows.Next() {
 		if err := rows.Scan(&count); err != nil {
@@ -24,12 +23,8 @@ func isExtensionEnabled(conn *performanceDbConnection.PGSQLConnection, extension
 	if err := rows.Err(); err != nil {
 		log.Error(err.Error())
 	}
-	if count > 0 {
-		isEnable = true
-	} else {
-		isEnable = false
-	}
-	return isEnable, nil
+
+	return count > 0, nil
 }
 
 func CheckPgWaitSamplingExtensionEnabled(conn *performanceDbConnection.PGSQLConnection) (bool, error) {
